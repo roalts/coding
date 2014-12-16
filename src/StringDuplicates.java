5be1600f9cdfbffee18d6c18413919b8aa5ea231
Raghav Apoorv <raghav.apoorv@gmail.com>
2014-12-16 19:22:19 +0530

    Question3

---
+++ src/StringDuplicates.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class StringDuplicates {
+
+	public static String checkDuplicate(String input){
+		String s = " ";
+		for(int i=0; i<input.length()-1; i++){
+			if(input.charAt(i)==input.charAt(i+1)){
+				 s = s+ input.charAt(i);
+					i++;
+			}
+			else
+				s= s+ input.charAt(i);
+		}
+		return s;
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		String output = checkDuplicate(input);
+		System.out.print(output);
+	}
+
+}
