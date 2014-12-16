ae70a3def65fb7d62eb1815029685ed3f9eabefc
Raghav Apoorv <raghav.apoorv@gmail.com>
2014-12-17 00:32:58 +0530

    minor changes

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -21,7 +21,6 @@ public class ReverseArray {
 	public static void printArray(int[] input){
 		for(int i=0; i<input.length; i++){
 			System.out.print(input[i]+"\t");
-
 		}
 		System.out.print("\n");
 	}
@@ -35,12 +34,5 @@ public class ReverseArray {
 			userInput[j]=temp;
 			printArray(userInput);
 		}
-
-
-
-
 	}
-
-
-
 }
