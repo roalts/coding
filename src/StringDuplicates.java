b9b2577e794c60767e81181a656446614ebbbbcf
Raghav Apoorv <raghav.apoorv@gmail.com>
2014-12-16 23:19:18 +0530

    last element getting iterated
    not a good solution

--- src/StringDuplicates.java
+++ src/StringDuplicates.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 public class StringDuplicates {
 
 	public static String checkDuplicate(String input){
-		String s = " ";
-		for(int i=0; i<input.length()-1; i++){
-			if(input.charAt(i)==input.charAt(i+1)){
-				 s = s+ input.charAt(i);
+		String s = "";
+		for(int i=0; i<input.length(); i++){
+			if(i!=input.length()-1){
+				if(input.charAt(i)==input.charAt(i+1)){
+					s = s+ input.charAt(i);
 					i++;
+				}
+
+				else
+					s= s + input.charAt(i);
 			}
-			else
-				s= s+ input.charAt(i);
+			else 
+				s= s+input.charAt(i);
 		}
 		return s;
-		
+
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
